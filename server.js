7855962d5e3d0b58f4d08a956d03f37410b6b8ce
Artan <artanpl03@gmail.com>
2023-01-28 15:29:18 -0500

    added passport local

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
+const passport = require("passport")
 
 mongoose.connect("mongodb://localhost/bodyboostdb" );
 
