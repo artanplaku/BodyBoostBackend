fc32ec41bd8af013e81b37d1640203c02b51ccf8
Artan <artanpl03@gmail.com>
2023-01-28 15:16:59 -0500

    installed passport and passport jwt

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost/bodyboostdb" );
+
+const db = mongoose.connection;
+
+db.on("connected", () => {
+    console.log("Mongoose default connection is open")
+})
 
 app.get('/', (req, res) =>{
     res.send('Hello, World')
