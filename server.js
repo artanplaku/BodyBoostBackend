9a8729bb17d92245103c59825c03c3cd3897ed3a
Artan <artanpl03@gmail.com>
2023-08-10 12:52:37 -0400

    Revert "Add chat functionality to server"
    This reverts commit 71f939cfb223764b404fc81e1263c344846146d1.

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const protectedRouter = require('./routes/protectedRouter');
 const imageRoutes = require('./routes/ImageRoutes');
 const contractRoutes = require('./routes/ContractRoutes') 
 const checkToken = require('./checkToken')
-const chatRoutes = require('./routes/chatRoutes')
 require('dotenv').config();
 
 const app = express();
@@ -39,7 +38,6 @@ app.use('/api/protected', checkToken, protectedRouter);
 app.use('/api/images', imageRoutes);
 app.use('/uploads', express.static('uploads'));
 app.use('/api/contracts', contractRoutes);
-app.use('/api/chat', chatRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
